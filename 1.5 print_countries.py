d6d87fb10df0f52385723e0c97430eb1498f3522
Aleksandra Korniak <ola.korniak@gmail.com>
2024-12-13 12:47:18 +0100

    Update 1.5 print_countries.py

--- 1.5 print_countries.py
+++ 1.5 print_countries.py
@@ -3,4 +3,4 @@
 #
 with open('countries.txt', 'r') as file:
     for i, line in enumerate(file , 1):
-        print(f'{i}, {line}', end="") # end=""due to the existence of end-of-line characters at the end of lines in the text file
+        print(f'{i}. {line}', end="") # end=""due to the existence of end-of-line characters at the end of lines in the text file
